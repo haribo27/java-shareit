02f57dff7e1c2b029cb593973602d3d9d7899242
haribo52 <alexeizubtsoff@yandex.ru>
2024-08-31 09:34:37 +0400

    first commit

---
+++ src/main/java/ru/practicum/shareit/user/dto/NewUserRequest.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class NewUserRequest {
+
+    private long id;
+    @NotNull
+    @NotBlank
+    private String name;
+    @NotNull
+    @NotBlank
+    private String email;
+}
