aac1fc4d2a7a4e002d3cc9e24f0700d4f9d8aa18
haribo52 <alexeizubtsoff@yandex.ru>
2024-10-10 11:25:27 +0400

    checkstyle

--- gateway/src/main/java/ru/practicum/shareit/gateway/user/UserClient.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/user/UserClient.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.gateway.user;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
